daeba403b21ecf6256e08660ec7070cf71f0580b
geocohn <geo@creationgroundmedia.com>
2017-03-11 15:48:37 -0800

    refactor: Use ToolBar as ActionBar

--- app/src/main/java/com/creationgroundmedia/week2/MainActivity.java
+++ app/src/main/java/com/creationgroundmedia/week2/MainActivity.java
@@ -3,6 +3,7 @@ package com.creationgroundmedia.week2;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -13,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
     @Override
