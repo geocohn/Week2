238c9fdce6472ea56cff26246d2ea3dfbc285af4
geocohn <geo@creationgroundmedia.com>
2017-03-11 15:22:47 -0800

    feat: Add menu item using XML onClick

--- app/src/main/java/com/creationgroundmedia/week2/MainActivity.java
+++ app/src/main/java/com/creationgroundmedia/week2/MainActivity.java
@@ -2,6 +2,9 @@ package com.creationgroundmedia.week2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public void onAdd(MenuItem item) {
+        Toast.makeText(this, "Menu item clicked", Toast.LENGTH_SHORT).show();
     }
 }
