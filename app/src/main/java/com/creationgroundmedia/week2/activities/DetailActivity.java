74ae832d06bdc326d968a91aa61570c2f9ff796a
geocohn <geo@creationgroundmedia.com>
2017-03-12 17:12:39 -0700

    feat: Add simple Share to DetailActivity menu using implicit intent

--- app/src/main/java/com/creationgroundmedia/week2/activities/DetailActivity.java
+++ app/src/main/java/com/creationgroundmedia/week2/activities/DetailActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebView;
 import android.widget.Button;
@@ -23,6 +25,7 @@ import org.w3c.dom.Text;
 public class DetailActivity extends AppCompatActivity {
 
     private static final String ARTICLE = "article";
+    private Article mArticle;
 
     public static void start(Context context, Article article) {
         Intent intent = new Intent(context, DetailActivity.class);
@@ -41,30 +44,55 @@ public class DetailActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
 
-        final Article article = getIntent().getParcelableExtra(ARTICLE);
+        mArticle = getIntent().getParcelableExtra(ARTICLE);
 
         TextView tvHeadline = (TextView) findViewById(R.id.tvHeadline);
         ImageView ivThumb = (ImageView) findViewById(R.id.ivThumb);
         TextView tvSnippet = (TextView) findViewById(R.id.tvSnippet);
         Button btViewArticle = (Button) findViewById(R.id.btViewArticle);
 
-        if (TextUtils.isEmpty(article.getImageUrl())) {
+        if (TextUtils.isEmpty(mArticle.getImageUrl())) {
             ivThumb.setVisibility(View.GONE);
         } else {
             ivThumb.setVisibility(View.VISIBLE);
-            Picasso.with(this).load(article.getImageUrl()).into(ivThumb);
+            Picasso.with(this).load(mArticle.getImageUrl()).into(ivThumb);
         }
 
-        tvHeadline.setText(article.getHeadline());
-        tvSnippet.setText(article.getSnippet());
+        tvHeadline.setText(mArticle.getHeadline());
+        tvSnippet.setText(mArticle.getSnippet());
 
         btViewArticle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(Intent.ACTION_VIEW);
-                intent.setData(Uri.parse(article.getWebUrl()));
+                intent.setData(Uri.parse(mArticle.getWebUrl()));
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_detail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.miShare:
+                share(mArticle);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void share(Article article) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_SUBJECT, article.getHeadline());
+        intent.putExtra(Intent.EXTRA_TEXT, article.getWebUrl() + "\n\nSent via The Best Android App Ever");
+        startActivity(intent);
     }
 }
